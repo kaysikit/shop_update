dc98a5332e63e5058a76dfd31ec5e00b230fdfff
kaysikit <kaysik_it@mail.ru>
2021-11-18 23:36:42 +0500

    Now you can authorize

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,12 +2,13 @@ from django.db import transaction
 from django.shortcuts import render
 from django.contrib.contenttypes.models import ContentType
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect
 from django.views.generic import DetailView, View
 
 from .models import Category, Customer, Cart, CartProduct, Product
 from .mixins import CartMixin
-from .forms import OrderForm
+from .forms import OrderForm, LoginForm
 from .utils import recalc_cart
 
 
@@ -146,3 +147,25 @@ class MakeOrderView(CartMixin, View):
             return HttpResponseRedirect('/')
         return HttpResponseRedirect('/checkout/')
 
+class LoginView(CartMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        form = LoginForm(request.POST or None)
+        categories = Category.objects.all()
+        context = {
+            'form': form,
+            'categories': categories,
+            'cart': self.cart
+        }
+        return render(request, 'login.html', context)
+
+    def post(self, request, *args, **kwargs):
+        form = LoginForm(request.POST or None)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return HttpResponseRedirect('/')
+        return render(request, 'login.html', {'form': form, 'cart': self.cart})
