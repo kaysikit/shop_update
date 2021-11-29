98e8521c6ab1d131776a212886d99b75ea6f2180
kaysikit <kaysik_it@mail.ru>
2021-11-29 21:48:55 +0500

    Added profile page Displayed information about orders in the profile Created a button for additional information for the user

--- mainapp/views.py
+++ mainapp/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect
 from django.views.generic import DetailView, View
 
-from .models import Category, Customer, Cart, CartProduct, Product
+from .models import Category, Customer, Order, CartProduct, Product
 from .mixins import CartMixin
 from .forms import OrderForm, LoginForm, RegistrationForm
 from .utils import recalc_cart
@@ -204,3 +204,16 @@ class RegistrationView(CartMixin, View):
             return HttpResponseRedirect('/')
         context = {'form': form, 'cart': self.cart}
         return render(request, 'registration.html', context)
+
+
+class ProfileView(CartMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        customer = Customer.objects.get(user=request.user)
+        orders = Order.objects.filter(customer=customer).order_by('-created_at')
+        categories = Category.objects.all()
+        return render(
+            request,
+            'profile.html',
+            {'orders': orders, 'cart': self.cart, 'categories': categories}
+        )
